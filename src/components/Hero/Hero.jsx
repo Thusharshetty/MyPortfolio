95946371c4588f096a3399deeaf5bb652248be1d
Thushar shetty <shettythushar31@gmail.com>
2026-06-26 09:19:52 +0530

    Navbar

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Hero.css'
+
+export default function Hero() {
+  return (
+    <section className="hero">
+
+      <p className="tag">
+        Full Stack Developer
+      </p>
+
+      <h1>
+        Hi, I'm <br />
+        <span>Thushar Shetty</span>
+      </h1>
+
+      <p className="description">
+        Building scalable web applications and solving
+        problems with Java, React and Node.js.
+      </p>
+
+      <button>
+        View My Work
+      </button>
+
+    </section>
+  )
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <div>
-      Hi from NAvbar
-    </div>
+     <nav className="navbar">
+
+      <div className="logo">
+        TS
+      </div>
+
+      <ul className="navLinks">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+
+      <button className="contactBtn">
+        Get In Touch
+      </button>
+
+    </nav>
   )
 }

85c08703745a8e7947847d760c4a1cc390d0d35c
Thushar shetty <shettythushar31@gmail.com>
2026-06-27 17:29:59 +0530

    hero-3

--- src/components/Hero/HeroImage.jsx
+++ src/components/Hero/HeroImage.jsx
@@ -3,6 +3,7 @@ import Badge from "../ui/Badge/Badge";
 import { techBadges } from "@/data";
 import { motion } from "framer-motion";
 import { scaleIn } from "@/animations/variants";
+import Tilt from "react-parallax-tilt";
 
 export default function HeroImage() {
   return (
@@ -11,24 +12,33 @@ export default function HeroImage() {
       <div className="absolute top-1/2 left-1/2 h-[500px] w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[var(--accent)] opacity-15 blur-[130px]" />
 
       <div className="relative z-10 h-[560px] w-[420px]">
-        <motion.div variants={scaleIn}
-    initial="hidden"
-    animate="visible" className="relative h-full w-full -mt-3">
-          <Image
-            src="/images/Profilenew1.png"
-            alt="Thushar Shetty"
-            fill
-            priority
-            className="select-none object-contain"
-          />
-        </motion.div>
-
+        <Tilt tiltMaxAngleX={6}
+          tiltMaxAngleY={6}
+          perspective={1200}
+          transitionSpeed={1800}
+          scale={1.01}
+          gyroscope={true}
+          className="relative h-full w-full" >
+          <motion.div variants={scaleIn}
+            initial="hidden"
+            animate="visible" className="relative h-full w-full -mt-3">
+            <Image
+              src="/images/Profilenew1.png"
+              alt="Thushar Shetty"
+              fill
+              priority
+              className="select-none object-contain"
+            />
+          </motion.div>
+        </Tilt>
         {techBadges.map((badge) => (
           <Badge
             key={badge.name}
             icon={badge.icon}
             name={badge.name}
             className={badge.className}
+            delay={badge.delay}
+            duration={badge.duration}
           />
         ))}
       </div>
--- src/components/ui/Badge/Badge.jsx
+++ src/components/ui/Badge/Badge.jsx
@@ -1,13 +1,25 @@
+import { motion } from "framer-motion";
 
-export default function Badge({ icon: Icon, name,color ,className = "" }) {
+
+export default function Badge({ icon: Icon, name,duration,delay,className = "" }) {
   return (
-    <div
-      className={`absolute flex items-center gap-2 rounded-full bg-white/90 px-4 py-2 shadow-lg border border-neutral-200 backdrop-blur-md transition-all duration-300 hover:-translate-y-1 ${className}`}
+    <motion.div
+    animate={{
+        y:[0,-25,0]
+    }}
+
+    transition={{
+        duration,
+        repeat:Infinity,
+        delay,
+        ease:"easeInOut"
+    }}
+      className={`absolute flex items-center gap-2  rounded-full bg-white/90 px-4 py-2 shadow-lg border border-neutral-200 backdrop-blur-md transition-all duration-300 hover:-translate-y-1 ${className}`}
     >
       <Icon className="text-lg"/>
       <span className="text-sm font-medium text-[var(--text)]">
         {name}
       </span>
-    </div>
+    </motion.div>
   );
 }

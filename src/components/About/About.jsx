2652ddca8e0bf4fe707ace364ddeaeee9f220d7e
Thushar shetty <shettythushar31@gmail.com>
2026-07-01 06:44:58 +0530

    about

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutContent from './AboutContent'
+import AboutFacts from './AboutFacts'
+
+export default function About() {
+  return (
+    <section>
+      <AboutContent/>
+      <AboutFacts/>
+    </section>
+  )
+}
---
+++ src/components/About/AboutContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutContent() {
+  return (
+    <div>
+      content
+    </div>
+  )
+}
